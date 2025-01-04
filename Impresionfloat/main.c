d8e91216a035b25103ac5c53c99795d38ee43863
Frandevep <frandevep@gmail.com>
2025-01-04 00:03:24 -0300

    Add files via upload

---
+++ Impresionfloat/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int num=10; //dar valor numerico a una variable, num es el vector de la variable int
+    float numdecimal=2.33; //permite dar valor a una variable decimal
+
+    printf("%d",num); // imprimir esa variable con valor numerico
+    printf("\n");//genera salto de linea para visualizar mejor la impresion
+    printf("%f",numdecimal); //imprime variable decimal
+
+
+    return 0;
+}
